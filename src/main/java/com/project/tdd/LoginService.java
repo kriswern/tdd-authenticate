c6bf0a7188ad9f06bf2da4ed71dc86dbe99a742b
Coin-byte <kriswern@icloud.com>
2021-11-18 15:29:07 +0100

    1.1 test success

--- src/main/java/com/project/tdd/LoginService.java
+++ src/main/java/com/project/tdd/LoginService.java
@@ -2,10 +2,12 @@ package com.project.tdd;
 
 public class LoginService {
 
+    protected String user = "annalosen";
+
     public LoginService() {
     }
 
     public boolean login(String userName, String password){
-        return false;
+        return user.equals(userName+password);
     }
 }
